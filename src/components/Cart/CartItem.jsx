f10600a3d52b99272ae9d1293fb6370cfaf6e954
kris015 <cvetinovickristijan8@gmail.com>
2025-06-26 20:34:18 +0200

    Novo

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -6,7 +6,10 @@ function CartItem({ item, index, updateQuantity, removeFromCart, selectedCurrenc
   const price = product.prices.find(p => p.currency.label === selectedCurrency);
 
   return (
-    <div className="cart-item">
+    <div 
+      className="cart-item" 
+      data-testid={`cart-item-attribute-${product.id}`} // Dodato glavno obeležje
+    >
       <div className="cart-item-info">
         <h4>{product.brand}</h4>
         <h3>{product.name}</h3>
@@ -14,25 +17,34 @@ function CartItem({ item, index, updateQuantity, removeFromCart, selectedCurrenc
           {price && `${price.currency.symbol}${price.amount.toFixed(2)}`}
         </div>
         <div className="cart-item-attributes">
-          {product.attributes.map(attr => (
-            <div 
-              key={attr.id}
-              className="cart-item-attribute"
-              data-testid={`cart-item-attribute-${attr.name.toLowerCase().replace(' ', '-')}`}
-            >
-              <h5>{attr.name}:</h5>
-              {attr.type === 'swatch' ? (
-                <div 
-                  className="color-swatch"
-                  style={{ backgroundColor: attr.items.find(i => i.id === selectedAttributes[attr.id])?.value }}
-                ></div>
-              ) : (
-                <div className="attribute-value">
-                  {attr.items.find(i => i.id === selectedAttributes[attr.id])?.displayValue}
-                </div>
-              )}
-            </div>
-          ))}
+          {product.attributes.map(attr => {
+            const selectedValue = attr.items.find(i => i.id === selectedAttributes[attr.id]);
+            const selectedDisplayValue = selectedValue?.displayValue || '';
+            
+            return (
+              <div 
+                key={attr.id}
+                className="cart-item-attribute"
+                data-testid={`cart-item-attribute-${attr.name.toLowerCase().replace(' ', '-')}`}
+              >
+                <h5>{attr.name}:</h5>
+                {attr.type === 'swatch' ? (
+                  <div 
+                    className="color-swatch"
+                    style={{ backgroundColor: selectedValue?.value }}
+                    data-testid={`cart-item-attribute-${attr.name.toLowerCase().replace(' ', '-')}-${selectedDisplayValue.toLowerCase().replace(' ', '-')}-selected`}
+                  ></div>
+                ) : (
+                  <div 
+                    className="attribute-value"
+                    data-testid={`cart-item-attribute-${attr.name.toLowerCase().replace(' ', '-')}-${selectedDisplayValue.toLowerCase().replace(' ', '-')}-selected`}
+                  >
+                    {selectedDisplayValue}
+                  </div>
+                )}
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="cart-item-quantity">
