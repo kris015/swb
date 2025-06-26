84e0fe99e7903172d48b39490940d01347ff5468
kris015 <cvetinovickristijan8@gmail.com>
2025-06-26 13:18:54 +0200

    za vercel2

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost/scandiweb-store/backend/index.php', // Update with your backend URL
+  uri: 'https://36c3-62-4-44-148.ngrok-free.app/scandiweb-store/backend/', // Update with your backend URL
   cache: new InMemoryCache()
 });
 
