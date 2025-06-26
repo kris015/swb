d97a1e3e2b79be07cc8ac5896ae0054156cdb446
kris015 <cvetinovickristijan8@gmail.com>
2025-06-26 22:33:25 +0200

    NovoTest

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -2,49 +2,56 @@ import React from 'react';
 import './styles.css';
 
 function CartItem({ item, index, updateQuantity, removeFromCart, selectedCurrency }) {
-  const price = item.product.prices.find(p => p.currency.label === selectedCurrency);
-  const amount = price ? price.amount : 0;
-
-  const decrease = () => {
-    if (item.quantity > 1) {
-      updateQuantity(index, item.quantity - 1);
-    } else {
-      removeFromCart(index);
-    }
-  };
-
-  const increase = () => {
-    updateQuantity(index, item.quantity + 1);
-  };
+  const { product, selectedAttributes, quantity } = item;
+  const price = product.prices.find(p => p.currency.label === selectedCurrency);
 
   return (
-    <div className="cart-item" data-testid={`cart-item-attribute-${item.product.id}`}>
-      <img src={item.product.gallery[0]} alt={item.product.name} />
-      <div className="cart-item-details">
-        <h4>{item.product.brand}</h4>
-        <h3>{item.product.name}</h3>
+    <div className="cart-item">
+      <div className="cart-item-info">
+        <h4>{product.brand}</h4>
+        <h3>{product.name}</h3>
         <div className="cart-item-price">
           {price && `${price.currency.symbol}${price.amount.toFixed(2)}`}
         </div>
-        {Object.entries(item.selectedAttributes).map(([attrId, itemId], i) => {
-          const attributeName = item.product.attributes.find(a => a.id === parseInt(attrId))?.name || '';
-          const displayValue = item.product.attributes
-            .find(a => a.id === parseInt(attrId))
-            ?.items.find(i => i.id === parseInt(itemId))?.displayValue || '';
-
-          return (
-            <div key={i} data-testid={`cart-item-attribute-${attributeName.toLowerCase().replace(/\s+/g, '-')}`}>
-              <span data-testid={`cart-item-attribute-${attributeName.toLowerCase().replace(/\s+/g, '-')}-${displayValue.toLowerCase().replace(/\s+/g, '-')}-selected`}>
-                {displayValue}
-              </span>
+        <div className="cart-item-attributes">
+          {product.attributes.map(attr => (
+            <div 
+              key={attr.id}
+              className="cart-item-attribute"
+              data-testid={`cart-item-attribute-${attr.name.toLowerCase().replace(' ', '-')}`}
+            >
+              <h5>{attr.name}:</h5>
+              {attr.type === 'swatch' ? (
+                <div 
+                  className="color-swatch"
+                  style={{ backgroundColor: attr.items.find(i => i.id === selectedAttributes[attr.id])?.value }}
+                ></div>
+              ) : (
+                <div className="attribute-value">
+                  {attr.items.find(i => i.id === selectedAttributes[attr.id])?.displayValue}
+                </div>
+              )}
             </div>
-          );
-        })}
-        <div className="quantity-controls">
-          <button data-testid="cart-item-amount-decrease" onClick={decrease}>-</button>
-          <span data-testid="cart-item-amount">{item.quantity}</span>
-          <button data-testid="cart-item-amount-increase" onClick={increase}>+</button>
+          ))}
         </div>
+      </div>
+      <div className="cart-item-quantity">
+        <button 
+          onClick={() => updateQuantity(index, quantity + 1)}
+          data-testid="cart-item-amount-increase"
+        >
+          +
+        </button>
+        <span data-testid="cart-item-amount">{quantity}</span>
+        <button 
+          onClick={() => updateQuantity(index, quantity - 1)}
+          data-testid="cart-item-amount-decrease"
+        >
+          -
+        </button>
+      </div>
+      <div className="cart-item-image">
+        <img src={product.gallery[0]} alt={product.name} />
       </div>
     </div>
   );
